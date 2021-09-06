6b413d99d47a66249eafb88175b5856c92197e5c
Asbimadelox <48446305+Asbimadelox@users.noreply.github.com>
2021-09-06 17:35:22 +0200

    Add files via upload

--- Zestaw 02/klient=zadanie2-7.py
+++ Zestaw 02/klient=zadanie2-7.py
@@ -6,18 +6,29 @@ Created on Mon Aug 16 18:14:48 2021
 """
 
 import socket
-import random
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((socket.gethostname(), 1234))
 
-adres = input("Wpisz adres pliku, który chcesz pobrać: ")
-
-'''
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-'''
-
-'''
-while 1:
+while True:
+    nazwa_pliku = input("Wpisz nazwe pliku, który chcesz pobrać: ")
     
-    
-'''
+    if nazwa_pliku != "":
+        sock.send(bytes(nazwa_pliku, "utf-8"))
+        port2 = sock.recv(1024)
+        port2 = int(port2.decode("utf-8"))
+        sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock2.connect((socket.gethostname(), port2))
+        
+        while True:
+            plik = sock2.recv(1024).decode("utf-8")
+            file = open(nazwa_pliku,"w")
+            file.write(plik)
+            print("Sciagnieto!")
+            file.close()
+            sock2.close()
+            break
+    else:
+        continue
+
+
--- Zestaw 02/server=zadanie2-7.py
+++ Zestaw 02/server=zadanie2-7.py
@@ -5,3 +5,46 @@ Created on Mon Aug 16 18:14:25 2021
 @author: Modecom
 """
 
+import socket
+import random
+import os.path
+#import requests
+import sys
+
+proto = socket.getprotobyname('tcp')
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto)
+sock.bind((socket.gethostname(), 1234))
+print("Serwer się uruchamia")
+sock.listen(5)
+
+
+while True:
+    client,addr = sock.accept()
+    
+    while True:
+        msg = client.recv(1024)
+        msg = msg.decode("utf-8")
+        print("Sprawdzanie, czy plik istnieje...")
+        
+        if os.path.exists(msg):
+            xyz = random.randint(1000,65535)
+            xyz = str(xyz)
+            client.send(bytes(xyz, "utf-8"))
+            sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto)
+            xyz = int(xyz)
+            sock2.bind((socket.gethostname(), xyz))
+            print("Serwer się przygotowuje do wysyłania pliku...")
+            sock2.listen(5)
+            
+            while True: 
+                client2, addr2 = sock2.accept()
+                file = open(msg, "r")
+                data = file.read()
+                client2.send(data.encode("utf-8"))
+                client2.close()
+                break
+            
+            client.close()
+            sys.exit(0)
+        else:
+            continue
