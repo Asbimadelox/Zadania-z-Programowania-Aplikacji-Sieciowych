49c984889122e7b2aebadb2d4aa30059cb824de6
Asbimadelox <48446305+Asbimadelox@users.noreply.github.com>
2021-09-06 19:51:35 +0200

    Add files via upload

--- Zestaw 03/klient=zadanie3-1.py
+++ Zestaw 03/klient=zadanie3-1.py
@@ -1,15 +1,31 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    s.connect(('localhost', 1769))
-    while True:
-        data = "Hello World"
-        s.send(data.encode('utf-8'))
-        data = s.recv(1024)
-        if not data:
-            break
-        print ('Response:' + data.decode('utf-8'))
-    s.close()
-except socket.error:
-    print ('Error')
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect((socket.gethostname(), 1769))
+
+while True:
+    bufor = input("Wpisz haslo do serwera: ")
+    
+    if bufor != "":
+        sock.send(bytes(bufor, "utf-8"))
+        bufor2 = sock.recv(1024).decode("utf-8")
+        if str(bufor2) == "Haslo prawidlowe!":
+            port2 = sock.recv(1024).decode("utf-8")
+            port2 = int(port2)
+            sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock2.connect((socket.gethostname(), port2))
+            
+            while True:
+                wiad = sock2.recv(1024).decode("utf-8")
+                print(wiad)
+                sock2.close()
+                break
+            
+        else:
+            print(str(bufor2))
+            print(" ")        
+    else:
+        continue
+    
+
--- Zestaw 03/server=zadanie3-1.py
+++ Zestaw 03/server=zadanie3-1.py
@@ -1,42 +1,43 @@
 import random
 import socket
 
-zgoda = False
 password = "Velvet"
 proto = socket.getprotobyname('tcp')
 
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM, proto)
 s.bind((socket.gethostname(), 1769))
 s.listen(5)
 
 while True:
-    
-    
     client, addr = s.accept()
-    bufor = client.recv(1024)
-    bufor = bufor.decode("utf-8")
     
-    if bufor == password:
-        zgoda = True
-        client.send(bytes(zgoda, ))
-        s.close()
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-        
-        
-
-while True:
-    client, addr = s.accept()
-    print("Connected: " + addr[0])
-
     while True:
-        data = client.recv(1024)
-        if not data:
-            break;
-        print ('I receive = ' + data.decode('utf-8'))
-        client.send(data)
+        #client.send(bytes("Prosze podac haslo: ", "utf-8"))
+        bufor = client.recv(1024)
+        bufor = bufor.decode("utf-8")
+        bufor = str(bufor)
     
-    client.close()
+        if bufor == password:
+            zgoda = True
+            client.send(bytes("Haslo prawidlowe!","utf-8" ))
+            xyz = random.randint(1000,65535)
+            xyz = str(xyz)
+            client.send(bytes(xyz, "utf-8"))
+            sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto)
+            xyz = int(xyz)
+            sock2.bind((socket.gethostname(), xyz))
+            print("Serwer się przygotowuje do wysłania wiadomosci...")
+            sock2.listen(5)
+        
+            while True:
+                client2, addr2 = sock2.accept()
+                wiad = "Ostatnie sztuki w żabce przy Instytucie Informatyki"
+                client2.send(wiad.encode("utf-8"))
+                client2.close()
+                break
+        else:
+            client.send(bytes("Błędne hasło!", "utf-8"))
 
-s.close()
+        
+        
